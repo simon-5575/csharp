007c25055b3f6d310798fef1fd8a94bc65d2f934
hlavacs24 <hlavacs24@Z00T1-09>
2024-09-17 09:39:19 +0200

    008 Hlavac urobene

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,21 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
 Console.WriteLine("Ahoj, " + meno);
+Console.WriteLine();
 
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
 Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine();    
+
+Console.WriteLine("Ake je tvoje oblubene jedlo?");
+string jedlo = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Hmm " + jedlo + " od gizky je dobry/a");
+
+Console.WriteLine($"Volam sa {meno},je mi {vek} a moje oblubene jedlo je {jedlo}");
+
+
+
+
