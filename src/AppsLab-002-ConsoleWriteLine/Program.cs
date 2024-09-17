2485d810bce9d031481f98af25146c19d8abddaf
hlavacs24 <hlavacs24@Z00T1-09>
2024-09-17 08:19:18 +0200

    Dokoncene cvicenie 005-Data Types

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string greetings = "Hello world";
+Console.WriteLine(greetings);
+
+
+greetings = "Goodbye world"
+Console.WriteLine (greetings);
+
+
+Console.WriteLine ("Hello + "Simon")
+
--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+int age = 15;
+string name = "Simon";
+
+Console.WriteLine("Simon");
+
+
+char inicialky = 'S';
+bool isAdult = false;
+float accountBalance= 125.25f;
+Console.WriteLine(accountBalance);
